a875539f81a6606cefbc60d235304fb041f4dc24
ildar-G <igazimov83@gmail.com>
2023-09-14 13:27:25 +0300

    Добавить def show_best_video в playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -30,6 +30,24 @@ class PlayList:
         url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
         return title, url
 
+    def get_video_details(self) -> List[dict]:
+        """
+        Получает детали видео для видео в плейлисте.
+        """
+        playlist_videos = youtube.playlistItems().list(
+            playlistId=self.playlist_id,
+            part='contentDetails',
+            maxResults=50,
+        ).execute()
+        video_ids = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+
+        video_response = youtube.videos().list(
+            part='contentDetails',
+            id=','.join(video_ids)
+        ).execute()
+
+        return video_response['items']
+
     def total_duration(self) -> datetime.timedelta:
         """
         Вычисляет общую продолжительность всех видео в плейлисте.
@@ -42,5 +60,26 @@ class PlayList:
         )
         return total_duration
 
+    def show_best_video(self) -> str:
+        """
+        Находит и возвращает URL видео с наибольшим количеством лайков в плейлисте.
+        """
+        video_details = self.get_video_details()
+        best_video_id = None
+        max_likes = 0
+
+        for video in video_details:
+            video_id = video['id']
+            video_response = youtube.videos().list(
+                part='statistics',
+                id=video_id
+            ).execute()
+            like_count = int(video_response['items'][0]['statistics']['likeCount'])
+            if like_count > max_likes:
+                max_likes = like_count
+                best_video_id = video_id
+
+        best_video_url = f"https://youtu.be/{best_video_id}"
+        return best_video_url
 
 
