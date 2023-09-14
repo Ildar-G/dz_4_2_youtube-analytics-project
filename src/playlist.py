6a3502e72eda4ad8dc5d459e567424c0d6d9da6d
ildar-G <igazimov83@gmail.com>
2023-09-14 13:06:51 +0300

    Добавить def get_playlist_info в playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -1,3 +1,12 @@
+import os
+import datetime
+import isodate
+from typing import List
+from googleapiclient.discovery import build
+
+api_key = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
 
 class PlayList:
     """
@@ -7,4 +16,19 @@ class PlayList:
     def __init__(self, playlist_id: str):
         self.playlist_id = playlist_id
         self.title, self.url = self.get_playlist_info()
+
     def get_playlist_info(self) -> tuple[str, str]:
+        """
+        Информация о видео для видео в плейлисте
+        """
+        playlist_response = youtube.playlists().list(
+            part='snippet',
+            id=self.playlist_id
+        ).execute()
+        playlist_info = playlist_response['items'][0]['snippet']
+        title = playlist_info['title']
+        url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
+        return title, url
+
+
+
